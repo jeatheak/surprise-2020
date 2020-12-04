299673820cf5223b5c9654cdb0b7dcefd411e46c
SS5582 <thomas.van.den.heuvel@engie.com>
2020-12-04 09:54:56 +0100

    wp

--- Code/MicroPython/Utils/stepper.py
+++ Code/MicroPython/Utils/stepper.py
@@ -1,7 +1,9 @@
-  
+
 import time
 
 # only test for uln2003
+
+
 class Stepper:
     FULL_ROTATION = int(4075.7728395061727 / 8)
 
@@ -22,9 +24,15 @@ class Stepper:
         [0, 1, 0, 1],
         [1, 0, 0, 1]
     ]
+
     def __init__(self, mode, pin1, pin2, pin3, pin4, delay):
         if mode == 'FULL_STEP':
             self.mode = self.FULL_STEP
+            self.pin1 = pin1
+            self.pin2 = pin2
+            self.pin3 = pin3
+            self.pin4 = pin4
+            self.delay = delay  # Recommend 10+ for FULL_STEP, 1 is OK for HALF_STEP
         else:
             self.mode = self.HALF_STEP
             self.pin1 = pin1
@@ -32,10 +40,10 @@ class Stepper:
             self.pin3 = pin3
             self.pin4 = pin4
             self.delay = delay  # Recommend 10+ for FULL_STEP, 1 is OK for HALF_STEP
-        
+
         # Initialize all to 0
         self.reset()
-        
+
     def step(self, count, direction=1):
         """Rotate count steps. direction = -1 means backwards"""
         for x in range(count):
@@ -46,14 +54,17 @@ class Stepper:
                 self.pin4(bit[3])
                 time.sleep_ms(self.delay)
         self.reset()
+
     def angle(self, r, direction=1):
-    	self.step(int(self.FULL_ROTATION * r / 360), direction)
+        self.step(int(self.FULL_ROTATION * r / 360), direction)
+
     def reset(self):
         # Reset to 0, no holding, these are geared, you can't move them
-        self.pin1(0) 
-        self.pin2(0) 
-        self.pin3(0) 
+        self.pin1(0)
+        self.pin2(0)
+        self.pin3(0)
         self.pin4(0)
 
+
 def create(pin1, pin2, pin3, pin4, delay=2, mode='HALF_STEP'):
-	return Stepper(mode, pin1, pin2, pin3, pin4, delay)
+    return Stepper(mode, pin1, pin2, pin3, pin4, delay)
--- Code/MicroPython/main.py
+++ Code/MicroPython/main.py
@@ -1,1 +1,26 @@
-import Scenes.Scenes
+#import Scenes.Scenes
+
+
+from Utils.stepper import create as StepperCreate
+from machine import Pin
+from neopixel import NeoPixel
+
+# print('init Pixel')
+# pinNeo = Pin(16, Pin.OUT)
+# neo = NeoPixel(pinNeo, 34)
+
+
+# for led in range(34):
+#     neo[led] = (25, 25, 25)
+
+
+# neo.write()
+# print('Done writing leds')
+
+print('start stepper')
+
+
+s1 = StepperCreate(Pin(27, Pin.OUT), Pin(25, Pin.OUT),
+                   Pin(32, Pin.OUT), Pin(12, Pin.OUT), delay=1)
+s1.step(500, -1)  # Open
+s1.step(500)  # close
